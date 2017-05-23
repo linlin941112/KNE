780e9a49c9d5752d161a9c9a6bf0c9a16b3a3d13
御狐神灬凛 <linlin941112@163.com>
2017-05-23 08:38:30 +0800

    添加2级3级页

---
+++ js/fencai.js
@@ -0,0 +1,22 @@
+$(function(){
+	var xiahua=document.querySelector("header nav ul .xiahua");
+	var navli=document.querySelectorAll("header nav ul li");
+	for (var i = 0; i < navli.length; i++) {
+		navli[i].w=navli[i].offsetWidth;
+		navli[i].l=navli[i].offsetLeft;
+	}
+	// 初始化
+	xiahua.style.left=navli[1].l+"px";
+	xiahua.style.width=navli[1].w+"px";
+
+	for (var i = 0; i < navli.length; i++) {
+		navli[i].index=i;
+		navli[i].onmouseover=function(){
+			xiahua.style.left=navli[this.index].l+"px";
+			xiahua.style.width=navli[this.index].w+"px";
+		}
+	}
+
+
+	
+})
